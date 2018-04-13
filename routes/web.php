65f3fed04e96d665c8091070be7e0e495d6c655e
haohexin <2554470320@qq.com>
2018-04-13 07:41:04 +0000

    Sidebar links

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
-
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
